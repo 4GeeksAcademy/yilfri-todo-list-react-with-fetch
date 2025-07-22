5aedc707ce35ef9a3bb7919830889ce8918b3c4d
Yilfri Salave <yilfri97@hotmail.com>
2025-07-22 16:26:33 +0000

    todo

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+
+	const [tasks, setTasks] = useState(["Make the bed", "Wash my hands", "Eat", "Walk the dog"])
+
+	function addTask(e) {
+		if (e.key == "Enter") {
+			setTasks([...tasks, e.target.value])
+			e.target.value = ''
+		}
+	}
+
+	function deleteTask(taskToDelete) {
+		setTasks(tasks.filter((_, index) => index !== taskToDelete));
+	}
+
 	return (
 		<div className="text-center">
-            
-
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<h1>TODOS</h1>
+			<div className="container list-group">
+				<input type="text" name="taskInput" placeholder="What needs to be done?" onKeyDown={(e) => addTask(e)} className="list-group-item" />
+				<ol style={{ listStyleType: "none", cursor: "default", padding: "0" }}>
+					{tasks.map((task, index) => (
+						<li key={index} className="list-group-item d-flex justify-content-between hidden-x">{task} <span onClick={() => deleteTask(index)}>X</span></li>
+					))}
+					<p className="list-group-item d-flex justify-content-start" style={{ fontSize: "10px", color: "gray" }}>{tasks.length} item{tasks.length > 1 ? "s" : ""} left</p>
+				</ol>
+			</div>
 		</div>
 	);
 };
