898a27e95d9fce34363eb079bb9ab6339526ae1e
Yilfri Salave <yilfri97@hotmail.com>
2025-07-28 05:39:22 +0000

    Todo with Fetch

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,35 +1,60 @@
-import React, { useState } from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useEffect, useState } from "react";
 
 //create your first component
 const Home = () => {
+	const [todos, setTodos] = useState([]);
 
-	const [tasks, setTasks] = useState(["Make the bed", "Wash my hands", "Eat", "Walk the dog"])
+	useEffect(() => {
+		createUser()
+		getTodos()
+	}, [])
 
-	function addTask(e) {
-		if (e.key == "Enter") {
-			setTasks([...tasks, e.target.value])
+	function createUser() {
+		fetch("https://playground.4geeks.com/todo/users/yilfri", { method: "POST", headers: { "Content-Type": "application/json" } })
+			.then(response => response.json())
+	}
+
+	function getTodos() {
+		fetch("https://playground.4geeks.com/todo/users/yilfri")
+			.then(response => response.json())
+			.then(data => { setTodos(data.todos) })
+	}
+
+	function addTodo(e) {
+		if (e.key == "Enter" && e.target.value !== '') {
+			fetch("https://playground.4geeks.com/todo/todos/yilfri", {
+				method: "POST", headers: { "Content-Type": "application/json" }, body: JSON.stringify({ label: e.target.value, is_done: false })
+			})
+				.then(responde => getTodos())
 			e.target.value = ''
 		}
 	}
 
-	function deleteTask(taskToDelete) {
-		setTasks(tasks.filter((_, index) => index !== taskToDelete));
+	function deleteTodo(taskToDelete) {
+		fetch(`https://playground.4geeks.com/todo/todos/${taskToDelete}`, {
+			method: "DELETE", headers: { "Content-Type": "application/json" }
+		})
+			.then(response => { getTodos() })
+	}
+
+	function deleteAllTodos() {
+		setTodos([])
+		fetch("https://playground.4geeks.com/todo/users/yilfri", { method: "DELETE" })
+			.then(response => createUser())
 	}
 
 	return (
 		<div className="text-center">
 			<h1>TODOS</h1>
 			<div className="container list-group">
-				<input type="text" name="taskInput" placeholder="What needs to be done?" onKeyDown={(e) => addTask(e)} className="list-group-item" />
+				<input type="text" name="taskInput" placeholder="What needs to be done?" onKeyDown={(e) => addTodo(e)} className="list-group-item" />
 				<ol style={{ listStyleType: "none", cursor: "default", padding: "0" }}>
-					{tasks.map((task, index) => (
-						<li key={index} className="list-group-item d-flex justify-content-between hidden-x">{task} <span onClick={() => deleteTask(index)}>X</span></li>
+					{todos.map((todo) => (
+						<li key={todo.id} className="list-group-item d-flex justify-content-between hidden-x">{todo.label} <span onClick={() => deleteTodo(todo.id)}>X</span></li>
 					))}
-					<p className="list-group-item d-flex justify-content-start" style={{ fontSize: "10px", color: "gray" }}>{tasks.length} item{tasks.length > 1 ? "s" : ""} left</p>
+					<p className="list-group-item d-flex justify-content-start" style={{ fontSize: "10px", color: "gray" }}>{todos.length} item{todos.length > 1 ? "s" : ""} left</p>
 				</ol>
+				<button onClick={deleteAllTodos}>Delete tasks</button>
 			</div>
 		</div>
 	);
